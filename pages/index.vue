3f3def06f083d10a759f66ac710b2e8b2b282221
Hafiz Javaid Iqbal <hafiz@rigrex-mac-hafiz.local>
2023-10-07 12:59:12 +0500

    Landing Page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,126 @@
 <template>
-  <div>Landing Page</div>
+  <div class="h-screen">
+    <main class="h-full overflow-auto">
+      <!-- Header -->
+      <header class="absolute inset-x-0 top-0 z-50">
+        <nav class="flex items-center justify-between max-w-7xl mx-auto py-4 px-6">
+          <NuxtLink href="/" class="flex items-center">
+            <div class="relative h-8 w-8 mr-4">
+              <img class="object-cover" alt="Logo" src="/logo.svg" />
+            </div>
+            <h1 class="text-2xl font-bold">Genius</h1>
+          </NuxtLink>
+          <div class="flex items-center gap-x-2">
+            <NuxtLink :to="user ? '/dashboard' : '/auth'">
+              <Button outline class="rounded-full"> Get Started </Button>
+            </NuxtLink>
+          </div>
+        </nav>
+      </header>
+      <!-- Hero -->
+      <div class="relative isolate px-6 pt-14 lg:px-8">
+        <div
+          class="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          aria-hidden="true"
+        >
+          <div
+            class="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
+            style="
+              clip-path: polygon(
+                74.1% 44.1%,
+                100% 61.6%,
+                97.5% 26.9%,
+                85.5% 0.1%,
+                80.7% 2%,
+                72.5% 32.5%,
+                60.2% 62.4%,
+                52.4% 68.1%,
+                47.5% 58.3%,
+                45.2% 34.5%,
+                27.5% 76.7%,
+                0.1% 64.9%,
+                17.9% 100%,
+                27.6% 76.8%,
+                76.1% 97.7%,
+                74.1% 44.1%
+              );
+            "
+          />
+        </div>
+        <div class="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+          <div class="text-center">
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
+              Data to enrich your online business
+            </h1>
+            <p class="mt-6 text-lg leading-8 text-gray-600">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem
+              cupidatat commodo. Elit sunt amet fugiat veniam occaecat fugiat aliqua.
+            </p>
+            <div class="mt-10 flex items-center justify-center gap-x-6">
+              <NuxtLink :to="user ? '/dashboard' : '/auth'">
+                <Button variant="premium"> Start Generating For Free </Button>
+              </NuxtLink>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Logos -->
+      <div class="bg-white pb-24 sm:pb-32">
+        <div class="mx-auto max-w-7xl px-6 lg:px-8">
+          <h2 class="text-center text-lg font-semibold leading-8 text-gray-900">
+            Trusted by the world’s most innovative teams
+          </h2>
+          <div
+            class="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5"
+          >
+            <img
+              class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              src="/logo1.svg"
+              alt="Transistor"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              src="/logo2.svg"
+              alt="Reform"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 max-h-12 w-full object-contain lg:col-span-1"
+              src="/logo3.svg"
+              alt="Tuple"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 max-h-12 w-full object-contain sm:col-start-2 lg:col-span-1"
+              src="/logo4.svg"
+              alt="SavvyCal"
+              width="158"
+              height="48"
+            />
+            <img
+              class="col-span-2 col-start-2 max-h-12 w-full object-contain sm:col-start-auto lg:col-span-1"
+              src="/logo5.svg"
+              alt="Statamic"
+              width="158"
+              height="48"
+            />
+          </div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const user = useSupabaseUser();
+definePageMeta({
+  layout: 'home',
+});
+</script>
 
 <style scoped></style>
